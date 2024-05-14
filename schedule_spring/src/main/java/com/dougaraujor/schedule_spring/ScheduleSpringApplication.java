41839526a12d3e5b85ca65cafe0a8f9a3c53edbd
Douglas Araujo <dougaraujor@gmail.com>
2024-05-13 21:44:07 -0300

    Inicio Projeto

---
+++ schedule_spring/src/main/java/com/dougaraujor/schedule_spring/ScheduleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dougaraujor.schedule_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleSpringApplication.class, args);
+	}
+
+}
---
+++ schedule_spring/src/test/java/com/dougaraujor/schedule_spring/ScheduleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dougaraujor.schedule_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
